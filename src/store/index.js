1f8c5c672d261af8bb5f1ad98a07f8988e7d9b04
Aleksei Iakovets <aleksei@winlx.com>
2017-08-11 00:29:05 +0300

    Added react-router-redux 5 alpha

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,20 @@
 import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
+import createHistory from 'history/createBrowserHistory';
+import { routerMiddleware } from 'react-router-redux';
 
 import reducer from '../reducers/index';
 import initialState from './initialState.json';
 
+export const history = createHistory();
+
+const historyMiddleware = routerMiddleware(history);
+
 const store = createStore(
   reducer,
   initialState,
-  composeWithDevTools(applyMiddleware(thunk)),
+  composeWithDevTools(applyMiddleware(thunk, historyMiddleware)),
 );
 
 export default store;
